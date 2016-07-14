c44491ac233fa467fcdb970498aafb447d922a7a
Grant Heath <heathgr@gmail.com>
2016-07-14 16:24:43 -0600

    redid the remote script.  the new way is better.

---
+++ remote.template.sh
@@ -0,0 +1,9 @@
+#to get your "npm run remote" command to work do the following:
+# 1. go to the google compute engine dashboard for the project
+# 2. find the instance you will be remoting into (there should only be one)
+# 3. click on the options for ssh and select "view gcloud command"
+# 4. copy this commmand
+# 5. in the terminal go to the root of "the-count" project
+# 6. create a remote.sh file
+# 7. paste the gcloud command from step 4 into this file
+# 8. run chmod u+x on remote.sh and bob's your uncle
